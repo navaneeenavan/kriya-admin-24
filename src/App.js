58fbbe11f24af6a0dd924b5ad2ca90b2bddc7aac
jeyam03 <64417749+jeyam03@users.noreply.github.com>
2023-03-14 02:06:36 +0530

    Login

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavRoutes from "./Navroutes";
+import Login from "./pages/Login";
 import PortalWrapper from "./pages/PortalWrapper";
 import "./styles/tailwind.output.css";
 
@@ -11,11 +12,12 @@ const App = () => {
       <Toaster />
       <BrowserRouter>
         <Routes>
+          <Route path="/login" element={<Login />} />
           <Route path="" element={<PortalWrapper />}>
             {NavRoutes.map((n) => (
               <Route path={n.href} element={n.element} />
             ))}
-            <Route path="*" element={<p>gfdgd</p>} />
+            <Route path="*" element={<p></p>} />
           </Route>
         </Routes>
       </BrowserRouter>
