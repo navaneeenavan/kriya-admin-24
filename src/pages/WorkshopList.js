f0a0712457cd40db748a1a187e6fbb89a1afa575
jeyam03 <64417749+jeyam03@users.noreply.github.com>
2023-03-17 07:36:09 +0530

    Workshop percentage

--- src/pages/WorkshopList.js
+++ src/pages/WorkshopList.js
@@ -1,60 +1,88 @@
 const workshops = [
   {
     "wid": "WKSP0001",
-    "workName": "IoT system design using Embedded C and Python"
+    "workName": "IoT system design using Embedded C and Python",
+    "alteredFee": 600,
+    "maxCount": "45",
   },
   {
     "wid": "WKSP0002",
-    "workName": "Mobile App Development"
+    "workName": "Mobile App Development",
+    "alteredFee": 800,
+    "maxCount": "60",
   },
   {
     "wid": "WKSP0003",
-    "workName": "Deep Learning: Unlocking the Full Potential"
+    "workName": "Deep Learning: Unlocking the Full Potential",
+    "alteredFee": 600,
+    "maxCount": "70",
   },
   {
     "wid": "WKSP0004",
-    "workName": "R Programming in Data Analytics"
+    "workName": "R Programming in Data Analytics",
+    "alteredFee": 600,
+    "maxCount": "60",
   },
   {
     "wid": "WKSP0005",
-    "workName": "Game Development"
+    "workName": "Game Development",
+    "alteredFee": 500,
+    "maxCount": "70",
   },
   {
     "wid": "WKSP0006",
-    "workName": "Sensor Interface and Integration"
+    "workName": "Sensor Interface and Integration",
+    "alteredFee": 800,
+    "maxCount": "70",
   },
   {
     "wid": "WKSP0007",
-    "workName": "IOT for Mechanical Applications"
+    "workName": "IOT for Mechanical Applications",
+    "alteredFee": 500,
+    "maxCount": "65",
   },
   {
     "wid": "WKSP0008",
-    "workName": "Nuts and Bots with Naveen"
+    "workName": "Nuts and Bots with Naveen",
+    "alteredFee": 250,
+    "maxCount": "100",
   },
   {
     "wid": "WKSP0009",
-    "workName": "Build your Custom Robot in ROS"
+    "workName": "Build your Custom Robot in ROS",
+    "alteredFee": 500,
+    "maxCount": "50",
   },
   {
     "wid": "WKSP0010",
-    "workName": "Creative Fusion"
+    "workName": "Creative Fusion",
+    "alteredFee": 400,
+    "maxCount": "65",
   },
   {
     "wid": "WKSP0011",
-    "workName": "Non-Destructive Testing"
+    "workName": "Non-Destructive Testing",
+    "alteredFee": 300,
+    "maxCount": "50",
   },
   {
     "wid": "WKSP0012",
-    "workName": "FPGA Based System Design"
+    "workName": "FPGA Based System Design",
+    "alteredFee": 400,
+    "maxCount": "70",
   },
   {
     "wid": "WKSP0013",
-    "workName": "Solaris (Workshop + Skywatch)"
+    "workName": "Solaris (Workshop + Skywatch)",
+    "alteredFee": 300,
+    "maxCount": "65",
   },
   {
     "wid": "WKSP0014",
-    "workName": "Tensorflow Application In Biotechnology"
-  }
+    "workName": "Tensorflow Application In Biotechnology",
+    "alteredFee": 350,
+    "maxCount": "64",
+  },
 ]
 
 export default workshops;
--- src/pages/WorkshopReg.js
+++ src/pages/WorkshopReg.js
@@ -9,6 +9,7 @@ const WorkshopReg = () => {
   const [workStats, setWorkStats] = useState(null);
   const [registrations, setRegistrations] = useState(false);
   const [workID, setWorkID] = useState(true);
+  const [percentage, setPercentage] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -33,6 +34,7 @@ const WorkshopReg = () => {
           onClick={() => {
             setWorkID(true);
             setRegistrations(false);
+            setPercentage(false);
 
             let temp = workStats;
             temp.sort((a, b) => a._id.localeCompare(b._id));
@@ -45,6 +47,7 @@ const WorkshopReg = () => {
           onClick={() => {
             setWorkID(false);
             setRegistrations(true);
+            setPercentage(false);
 
             let temp = workStats;
             temp.sort((a, b) => b.count - a.count);
@@ -52,6 +55,19 @@ const WorkshopReg = () => {
           }}
         >
           Registrations
+        </button>
+        <button className={`hidden lg:block px-6 py-2 rounded-full ${percentage ? "bg-[#3c3c3c] text-white" : "bg-[#eaeaea] text-[#303030]"} border-2 border-[#303030]`}
+          onClick={() => {
+            setWorkID(false);
+            setRegistrations(false);
+            setPercentage(true);
+
+            let temp = workStats;
+            temp.sort((a, b) => ((b.count)/(workshops.find((work) => b._id === work.wid).maxCount)) - ((a.count)/(workshops.find((work) => a._id === work.wid).maxCount)));
+            setWorkStats(temp);
+          }}
+        >
+          Percentage
         </button>
       </div>
       <div className="space-y-4 px-4 lg:px-0">
@@ -62,6 +78,10 @@ const WorkshopReg = () => {
             <p className="text-xs">Workshop Name</p>
           </div>
           <div className="text-xs text-center w-[15%]">Total Reg.</div>
+          <div className="hidden lg:flex flex-col items-center justify-center w-[15%]">
+            <div className="text-xs text-center">Max Count</div>
+            <div className="text-xs text-center">Percentage</div>
+          </div>
           <div className="text-xs text-center w-[15%]">PSG Reg.</div>
           <div className="text-xs text-center w-[15%]">Non PSG Reg.</div>
         </div>
@@ -72,16 +92,20 @@ const WorkshopReg = () => {
             <div className="flex items-center">
               <div className="text-xl mr-6 lg:mr-4 w-[5%]">{index + 1}</div>
               <button className="w-1/2 flex flex-col text-left"
-                  onClick={() => {
-                    navigate(`/work-details/${w._id}`);
-                  }}
-                >
+                onClick={() => {
+                  navigate(`/work-details/${w._id}`);
+                }}
+              >
                 <p className="text-xs">{w._id}</p>
                 <p className="">
                   {workshops.find((work) => w._id === work.wid).workName}
                 </p>
               </button>
               <div className="text-3xl lg:text-4xl text-center font-semibold w-[15%]">{w.count}</div>
+              <div className="hidden lg:flex flex-row items-end justify-center w-[15%] space-x-1">
+                <div className="text-2xl text-center font-semibold">{workshops.find((work) => w._id === work.wid).maxCount}</div>
+                <div className="text-sm text-center">{(((w.count) / (workshops.find((work) => w._id === work.wid).maxCount)) * 100).toPrecision(3)}%</div>
+              </div>
               <div className="text-3xl lg:text-4xl text-center font-semibold w-[15%]">{w.psgCount}</div>
               <div className="text-3xl lg:text-4xl text-center font-semibold w-[15%]">{w.count - w.psgCount}</div>
             </div>
