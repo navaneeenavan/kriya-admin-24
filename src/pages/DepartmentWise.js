b3e2a0084f34d7617bee8acffd922ab340cc4b87
tgashwinkumar <tgashwinkumar@gmail.com>
2023-03-21 12:11:13 +0530

    finsihed dept wise

--- src/pages/DepartmentWise.js
+++ src/pages/DepartmentWise.js
@@ -2,16 +2,15 @@ import React, { useEffect, useState } from "react";
 import Layout from "../components/Layout";
 import { fetchDeptWise } from "../API/calls";
 import { toast } from "react-hot-toast";
-
+import departments from "./DepartmentList.js";
 const DepartmentWise = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     toast.promise(fetchDeptWise(), {
       loading: "Loading...",
       success: (data) => {
-        console.log(data);
-        setData(data.data);
+        setData(data.data.deptWiseCount);
         return "Success!";
       },
       error: (err) => {
@@ -23,7 +22,42 @@ const DepartmentWise = () => {
 
   return (
     <Layout title={"Department Wise"}>
-      <div className="flex"></div>
+      {data.length > 0 ? (
+        <div className="grid grid-cols-[200px_1fr] gap-4">
+          {departments.map((dept, index) => {
+            return (
+              <React.Fragment>
+                <div className="">
+                  <p className="font-semibold text-sm">{dept}</p>
+                </div>
+                <div className="flex flex-wrap gap-4">
+                  {[1, 2, 3, 4, 5].map((item) => {
+                    return (
+                      <div className="w-fit">
+                        <div className="text-sm">year-{item}</div>
+                        <div className="text-4xl font-semibold">
+                          {data.find(
+                            (d) =>
+                              d._id.year === item && d._id.department === dept
+                          )
+                            ? data.find(
+                                (d) =>
+                                  d._id.year === item &&
+                                  d._id.department === dept
+                              ).count
+                            : 0}
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </React.Fragment>
+            );
+          })}
+        </div>
+      ) : (
+        <div className="">Loading...</div>
+      )}
     </Layout>
   );
 };
